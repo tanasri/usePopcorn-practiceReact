30c5b036af5c1095b2acde2960a9f606f2c5df9a
tanasri <tanawan95@gmail.com>
2024-03-14 12:15:44 +0700

    fix:api

--- src/App.jsx
+++ src/App.jsx
@@ -317,7 +317,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatch, watched }) {
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
